1b5059103bc4e52d9c2d526a3070b7e23fa15fb1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-19 14:59:10 +0530

    created header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+<!-- eslint-disable -->
+<div class="head">
+  <h1>Pokemon App</h1>
+  <div class="panelBtn">
+    <a href="#">+</a>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.head{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+h1{
+    font-size: 72px;
+}
+.panelBtn{
+  border: 5px solid #00a900;
+  padding: 4px;
+  height: 40px;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+}
+a{
+  text-decoration: none;
+  font-size: 72px;
+  color: #00a900;
+}
+
+</style>
