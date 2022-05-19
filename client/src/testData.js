3450d81ba29d6699fa9af33625b9ed7383300cba
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-19 23:36:05 +0530

    added some test data

---
+++ client/src/testData.js
@@ -0,0 +1,27 @@
+export default testData = [
+    {
+        name: 'Pikachu',
+        type: 'Electric',
+        power: 82
+    },
+    {
+        name: 'Bulbasaur',
+        type: 'Grass',
+        power: 78
+    },
+    {
+        name: 'Charmandar',
+        type: 'Fire',
+        power: 75
+    },
+    {
+        name: 'Squirtle',
+        type: 'Water',
+        power: 77
+    },
+    {
+        name: 'Onix',
+        type: 'Rock',
+        power: 71
+    }
+]
