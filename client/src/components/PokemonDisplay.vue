a6df16018f687c2cd0ee56c504afb2980561a33c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-20 20:31:55 +0530

    displayed all pokemon cards from testData

---
+++ client/src/components/PokemonDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- eslint-disable -->
+  <div class="pokemon-display">
+      <PokemonCard v-for="a in pokemonList" :name="a.name" :type="a.type" :power="a.power"/>
+  </div>
+</template>
+
+<script>
+import testData from '../testData'
+import PokemonCard from './PokemonCard.vue'
+
+export default {
+    data() {
+        return {
+            pokemonList: testData
+        };
+    },
+    components: { PokemonCard }
+}
+</script>
+
+<style scoped>
+.pokemon-display{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1%;
+}
+</style>
