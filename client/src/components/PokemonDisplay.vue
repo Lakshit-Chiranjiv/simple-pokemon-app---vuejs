001578123021156668a4d40d1e5a365334b697a4
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-01 08:48:50 +0530

    added search conditional display...but not yet working

--- client/src/components/PokemonDisplay.vue
+++ client/src/components/PokemonDisplay.vue
@@ -3,6 +3,7 @@
   <div class="pokemon-display">
       <PokemonCard v-for="a in pokemonList" :name="a.name" :type="a.type" :power="a.power" :openUpdateModal="openUpdateModal" :openDeleteModal="openDeleteModal" :hidePokemonList="hidePokemonList" :setUpdatePokemonName="setUpdatePokemonName" :setUpdatePokemonType="setUpdatePokemonType" :setUpdatePokemonPower="setUpdatePokemonPower" :setDeletePokemonName="setDeletePokemonName" :closeOpsPanel="closeOpsPanel"/>
   </div>
+  <button @click="chkclick">filter check</button>
 </template>
 
 <script>
@@ -10,11 +11,20 @@ import testData from '../testData'
 import PokemonCard from './PokemonCard.vue'
 
 export default {
-    props: ['openUpdateModal','openDeleteModal','hidePokemonList','setUpdatePokemonName','setUpdatePokemonType','setUpdatePokemonPower','setDeletePokemonName','closeOpsPanel'],
+    props: ['openUpdateModal','openDeleteModal','hidePokemonList','setUpdatePokemonName','setUpdatePokemonType','setUpdatePokemonPower','setDeletePokemonName','closeOpsPanel','activeSearchField','searchText'],
     data() {
         return {
-            pokemonList: testData
+            pokemonList: testData,
+            filterList: (this.activeSearchField === 'name')?
+                        testData.filter(p => p.name.toLowerCase().includes(this.searchText.toLowerCase())):
+                        testData.filter(p => p.type.toLowerCase().includes(this.searchText.toLowerCase()))
         };
+    },
+    methods: {
+        chkclick(){
+            // console.log(this.filterList);
+            console.log(this.searchText)
+        }
     },
     components: { PokemonCard }
 }
